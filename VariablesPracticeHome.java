04ca17bc57290c5fabf4abc669840753f3542ab2
elchinamirov <elcinemirov533@gmail.com>
2023-08-27 17:31:25 +0400

    ev tapsiriqi 1 ve 2
    practice home

---
+++ VariablesPracticeHome.java
@@ -0,0 +1,15 @@
+public class VariablesPracticeHome {
+    public static void main(String[] args) {
+        int age = 25;
+        age=25;
+            System.out.println("age : " + age);
+boolean hasJob = false;
+        System.out.println("hasJob : " + hasJob);
+        char studentGrade = 'C';
+                System.out.println("studentGrade : " + studentGrade);
+                double carPrice = 25000;
+                System.out.print("carPrice : " + carPrice);
+                String employeeName = "Melik";
+                System.out.println("employeeName : " + employeeName);
+    }
+}
---
+++ VariablesPracticeHome2.java
@@ -0,0 +1,22 @@
+public class VariablesPracticeHome2 {
+    public static void main(String[] args) {
+        int age = 7;
+        System.out.println(age);
+                age=14;
+                System.out.println(age);
+                byte b = 7;
+                short s = 7;
+                long l = 7;
+                double d = 7.9D;
+                float f = 7.9F;
+                char c = 'A';
+                boolean hasHome = true;
+                System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(hasHome);
+    }
+}
