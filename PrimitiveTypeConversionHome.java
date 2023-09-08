ffbea91cd3cb7f1cc39338ce799a52539be1ee14
elchinamirov <elcinemirov533@gmail.com>
2023-09-09 00:26:31 +0400

    ev isi1
    homework 1

---
+++ PrimitiveTypeConversionHome.java
@@ -0,0 +1,38 @@
+public class PrimitiveTypeConversionHome {
+        public static void main(String[] args) {
+            int a=55;
+            byte b=(byte) a;
+            short s=636;
+            byte c=(byte)s;
+            long l=458L;
+            short d=(short)l;
+            long g=92523635483L;
+            int i=(int)g;
+            double f=65787.3D;
+            int o=(int)f;
+            char j='q';
+            int w=(int)j;
+            int r=266;
+            char p=(char)r;
+
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(s);
+            System.out.println(c);
+            System.out.println(l);
+            System.out.println(d);
+            System.out.println(g);
+            System.out.println(i);
+            System.out.println(f);
+            System.out.println(o);
+            System.out.println(j);
+            System.out.println(w);
+            System.out.println(r);
+            System.out.println(p);
+
+        }
+
+
+    }
+
