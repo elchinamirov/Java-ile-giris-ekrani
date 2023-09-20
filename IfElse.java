94384572f942c90fe786873d2a20dcff061751f0
elchinamirov <elcinemirov533@gmail.com>
2023-09-20 21:16:11 +0400

    IfElse
    ifelse practice

---
+++ IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse {
+    public static void main(String [] args){
+        int x=10;
+        if(x<10){
+            System.out.println("x-in qiymeti 10-dan kicik ededdir");
+        }
+        if(x>10){
+            System.out.println("x ededi 10-dan boyukdur");
+        }
+        if(x==10){
+            System.out.println("x ededi 10-a beraberdir");
+        }
+
+        else{
+            System.out.println("x-in qiymeti 10-dan kicik deyil");
+        }
+    }
+}
