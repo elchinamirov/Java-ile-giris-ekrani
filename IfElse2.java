6a553b90ffe13cdcacfb380fd527f7a92045aaf2
elchinamirov <elcinemirov533@gmail.com>
2023-09-20 22:11:04 +0400

    IfElse2
    IfElse2 practice2

---
+++ IfElse2.java
@@ -0,0 +1,22 @@
+public class IfElse2 {
+    public static void main(String[] args) {
+        int point = 69;
+
+
+        if (point > 90) {
+            System.out.println("A");
+        } else if (point > 80) {
+            System.out.println("B");
+        } else if (point >70) {
+            System.out.println("C");
+        } else if (point >60) {
+            System.out.println("D");
+        } else if (point >50) {
+            System.out.println("E");
+        } else {
+            System.out.println("UGURSUZ");
+
+        }
+    }
+}
+
