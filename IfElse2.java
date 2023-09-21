c9d2ac0d3aeb49e33840a338236c9ee765c38798
elchinamirov <elcinemirov533@gmail.com>
2023-09-21 04:53:03 +0400

    Orpractice
    Or practice

--- IfElse2.java
+++ IfElse2.java
@@ -1,11 +1,11 @@
 public class IfElse2 {
     public static void main(String[] args) {
-        int point = 69;
+        int point = 83;
 
 
         if (point > 90) {
             System.out.println("A");
-        } else if (point > 80) {
+        } else if (point > 80 && 90>point) {
             System.out.println("B");
         } else if (point >70) {
             System.out.println("C");
---
+++ Orpractice.java
@@ -0,0 +1,14 @@
+public class Orpractice {
+    public static void main(String[] args) {
+        int point=54;
+        String name= "Elchin";
+        String sector= "az";
+        if(point==54 || name=="Ali" && sector=="az"){
+            System.out.println("Ugurlu");
+
+        }
+
+
+            }
+        }
+
