4c8dd385dbbe6094d387a77cc697f19070161a56
elchinamirov <elcinemirov533@gmail.com>
2023-09-21 05:33:59 +0400

    NotPraktika
    Not Praktika

---
+++ NotPraktika.java
@@ -0,0 +1,9 @@
+public class NotPraktika {
+    public static void main(String[] args) {
+        int point=54;
+        String name="Ali";
+        if(!(point<24) && !(name =="Elchin")){
+            System.out.println(" melumat dogrudur ");
+        }
+    }
+}
