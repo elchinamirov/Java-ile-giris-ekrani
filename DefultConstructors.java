81d0c8948982038401670a59be5b34ffc40ace34
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 18:35:05 +0400

    DefultConstructors
    Defult Constructors

---
+++ DefultConstructors.java
@@ -0,0 +1,13 @@
+public class DefultConstructors {
+    public static void main(String [] args){
+
+     Computer hp=new Computer();
+        Computer dell=new Computer();
+
+System.out.println(dell.price);
+
+Computer asus=new Computer();
+asus.brand="Asus";
+System.out.println(asus.brand);
+    }
+}
