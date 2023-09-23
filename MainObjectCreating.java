928c118746b75b8ab73af23a3ce9aef6999d9f85
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 17:42:22 +0400

    MainObjectCreating
    Main Object Creating

---
+++ MainObjectCreating.java
@@ -0,0 +1,16 @@
+public class MainObjectCreating {
+    public static void main(String [] args){
+        Car bmw=new Car();
+        bmw.color="Green";
+        bmw.speed=220;
+
+        Car kia=new Car();
+        kia.color="red";
+        kia.speed=300;
+
+        System.out.println("Bmw speed="+bmw.speed);
+        System.out.println("Kia speed="+kia.speed);
+    }
+
+    }
+
