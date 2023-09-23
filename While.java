221d248789d2e03bf7f3f1257083bb7037bb91dc
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 14:22:06 +0400

    While Practice
    While Practice

---
+++ While.java
@@ -0,0 +1,15 @@
+public class While {
+    public static void main(String[] args){
+        int sum=0;
+    int i = 0;
+    while(i<10){
+        i++;
+        sum += i;
+        System.out.println(i);
+
+    }
+    System.out.println("____________________");
+        System.out.println("sum = "+sum);
+
+    }
+}
