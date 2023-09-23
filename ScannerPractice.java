a75a3e0fd713a44c5c6cd709b8e29a698ae818b8
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 15:39:51 +0400

    ScannerPractice
    Scanner Practice

---
+++ ScannerPractice.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class ScannerPractice {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+     System.out.println("Salam,Adinizi yazin : ");
+        String name=scanner.nextLine();
+System.out.print("Memnun oldum, "+name);
+String thesecondquestion=scanner.nextLine();
+System.out.println("Necesen?");
+String thirdquestion=scanner.nextLine();
+System.out.println("Teshekkurler");
+scanner.close();
+
+
+    }
+
+
+    }
+
