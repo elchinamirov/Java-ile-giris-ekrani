392bbce1cde45318b785256b5e106595071b0e5e
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 08:53:13 +0400

    Ternary parctice
    Ternary practice

---
+++ TernaryPractice.java
@@ -0,0 +1,16 @@
+public class TernaryPractice {
+    public static void main(String [] args) {
+
+        String nationality = "American";
+
+        String permition = "let";
+
+        permition = nationality == ("Azerbaijanian") ? "let" : "prohibit" ;
+
+        System.out.println( permition );
+
+        }
+    }
+
+
+
