659fa0093952ecd45e8396ed026bfe3af791421f
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 12:07:54 +0400

    Dovrler practice
    dovrler practice

---
+++ Dovr.java
@@ -0,0 +1,10 @@
+public class Dovr {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b <= 10; b++) {
+                System.out.println(a+"*"+b+"="+(a*b));
+            }
+System.out.println("___________________");
+
+        }
+    }}
