4d7c270c086e94247dd556b215f3f981d97cd34b
elchinamirov <elcinemirov533@gmail.com>
2023-09-23 14:57:46 +0400

    DoWhile practice
    DoWhile Practice

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int age = 16;
+       do {
+            System.out.println(age);
+age++;
+        }
+        while(age>=18);
+
+
+    }}
