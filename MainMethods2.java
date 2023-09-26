975fac938f1efbddb14403eb3f90c8f7d954e1e4
elchinamirov <elcinemirov533@gmail.com>
2023-09-26 15:37:42 +0400

    MainMethods2
    Main Methods 2

---
+++ MainMethods2.java
@@ -0,0 +1,32 @@
+public class MainMethods2 {
+    public static void main(String[] args ){
+        MainMethods2 m=new MainMethods2();
+        MainMethods2 y=new MainMethods2();
+
+        y.YesIm();
+
+        System.out.println("HELLO");
+
+        m.SayHello();
+
+        System.out.println("HELLO");
+
+        m.SayHello();
+
+        System.out.println("HELLO");
+
+        m.SayHello();
+
+        System.out.println("HELLO");
+
+        y.YesIm();
+    }
+    public void SayHello(){
+
+        System.out.println("SALAM");
+    }
+
+    public void YesIm(){
+        System.out.println("YES IM");
+    }
+}
