8cc495b6ba73bb62588e59b6cd39e39f2a8e1a1e
elchinamirov <elcinemirov533@gmail.com>
2023-09-28 20:03:01 +0400

    MainPrimitiveDefaultValues
    Main Primitive Default Values

--- MainPrimitiveDefaultValues.java
+++ MainPrimitiveDefaultValues.java
@@ -3,6 +3,8 @@ import java.security.PublicKey;
 
 public class MainPrimitiveDefaultValues {
 int salary;
+boolean isDigit;
+char c;
 
     public static void main(String [] args){
         int age=20;
@@ -11,9 +13,10 @@ int salary;
       // System.out.println(h);
        h=4;
        System.out.println(h);
-
        MainPrimitiveDefaultValues m=new MainPrimitiveDefaultValues();
-       System.out.println(m.salary);
+        System.out.println(m.salary);
+        System.out.println(m.c);
+        System.out.println(m.isDigit);
     }
 
 }
