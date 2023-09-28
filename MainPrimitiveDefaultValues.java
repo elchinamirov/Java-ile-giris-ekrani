7e2eb436b71c324b74119f8c5be40fe552c29bd2
elchinamirov <elcinemirov533@gmail.com>
2023-09-28 19:55:46 +0400

    MainPrimitiveDefaultValues
    Main Primitive Default Values

---
+++ MainPrimitiveDefaultValues.java
@@ -0,0 +1,19 @@
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+import java.security.PublicKey;
+
+public class MainPrimitiveDefaultValues {
+int salary;
+
+    public static void main(String [] args){
+        int age=20;
+       System.out.println(age);
+       int h;
+      // System.out.println(h);
+       h=4;
+       System.out.println(h);
+
+       MainPrimitiveDefaultValues m=new MainPrimitiveDefaultValues();
+       System.out.println(m.salary);
+    }
+
+}
