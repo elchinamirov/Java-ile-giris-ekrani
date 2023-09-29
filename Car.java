681a34c8063399c7ed1dee1b650dba0d2948a812
elchinamirov <elcinemirov533@gmail.com>
2023-09-29 19:35:15 +0400

    MainVariableScopes
    Main Variable Scopes

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+
+    public void start() {
+        int a=4;
+        System.out.println(a);
+
+        System.out.println(speed);
+        System.out.println(age);
+    }
+    int speed;
+    int age=10;
+}
---
+++ MainVariableScopes.java
@@ -0,0 +1,21 @@
+public class MainVariableScopes {
+    public static void main(String [] args){
+Car kia=new Car();
+kia.start();
+kia.start();
+kia.start();
+kia.start();
+    }
+}
+
+
+//public class Car {
+//    public void start() {
+//        int a=4;
+//        System.out.println(a);
+//        System.out.println(speed);
+//        System.out.println(age);
+//    }
+//    int speed;
+//    int age=10;
+//    }
