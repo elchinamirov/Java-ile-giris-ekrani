ffcb0de1b062e3f829d000ac1cb0e7010661461d
elchinamirov <elcinemirov533@gmail.com>
2023-09-29 18:42:21 +0400

    MainReferenceDefaultValues
    Main Reference Default Values

---
+++ MainReferenceDefultValues.java
@@ -0,0 +1,9 @@
+public class MainReferenceDefultValues {
+    String g;
+    public static void main(String[] args){
+    //    String u;
+    //    System.out.println(u);
+        MainReferenceDefultValues m=new MainReferenceDefultValues();
+System.out.println(m.g);
+    }
+}
