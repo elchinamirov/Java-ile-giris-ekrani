424346ed138631220fb43dce32901abcba9108ec
elchinamirov <elcinemirov533@gmail.com>
2023-09-30 09:36:14 +0400

    MainObjectCodeBlocks
    Main Object Code Blocks

---
+++ MainObjectCodeBlocks.java
@@ -0,0 +1,13 @@
+public class MainObjectCodeBlocks {
+    {
+        System.out.println("object block-1");
+    }
+    {
+        System.out.println("object block-2");
+    }
+    public static void main(String[] args){
+        MainObjectCodeBlocks m1=new MainObjectCodeBlocks();
+        MainObjectCodeBlocks m2=new MainObjectCodeBlocks();
+
+    }
+}
