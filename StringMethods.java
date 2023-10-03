d0b75ba8eefc61da3d6a1350aa68c9ff36ca0cee
elchinamirov <elcinemirov533@gmail.com>
2023-10-03 16:34:30 +0400

    StringMethods
    String Methods

---
+++ StringMethods.java
@@ -0,0 +1,17 @@
+public class StringMethods {
+  public static void main(String [] args){
+      int age=12;
+      System.out.println(age);
+      String a=String.valueOf(age);
+      System.out.println(a.length());
+
+
+          StringMethods m1=new StringMethods();
+
+    StringMethods m2=new StringMethods();
+
+System.out.println(m1==m2);
+
+    m1=m2;
+    System.out.println(m1==m2);
+}}
