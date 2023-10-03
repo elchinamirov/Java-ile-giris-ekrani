e70fd5fbc081a04b4650d663dcd8a26dd2106d5a
elchinamirov <elcinemirov533@gmail.com>
2023-10-03 16:10:29 +0400

    StringPool
    String Pool

---
+++ StringPool.java
@@ -0,0 +1,7 @@
+public class StringPool {
+    public static void main(String [] args){}
+    StringPool m=new StringPool();
+    String name=new String("Elchin");
+    String n=new String("Elchin"); //String pool
+String n1=new String("Elchin");
+}
