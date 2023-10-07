b08320fb0610a98abef58765a3029e0db7d1ba1c
elchinamirov <elcinemirov533@gmail.com>
2023-10-07 18:49:35 +0400

    Formatlasdirma
    Formatlasdirma

---
+++ formatlasdirma.java
@@ -0,0 +1,15 @@
+public class formatlasdirma {
+    public static void main(String [] args) {
+
+        String name="Adil";
+
+        int age=12;
+
+        double amount= 54.78644;
+
+        //System.out.println("menim adim :" + name+ ", yasim :"+ age);
+
+        System.out.printf(" menim adim :\"%s\",\n yasim :\"%d\",\n maasim :\"%.2f\"", name,age,amount);
+    }
+
+}
