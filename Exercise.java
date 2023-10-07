9fa21059a03d9fec2cefac802d0ea34f056c019b
elchinamirov <elcinemirov533@gmail.com>
2023-10-07 19:00:12 +0400

    Exercise.ders10
    Exercise ders10

---
+++ Exercise.java
@@ -0,0 +1,7 @@
+public class Exercise {
+    public static void main(String [] args){
+        char [] c={'S','A','L','A','M'};
+        String s=String.copyValueOf(c);
+    System.out.println(s);
+    }
+}
