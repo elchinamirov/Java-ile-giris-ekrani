62d0a165a4c5a7bbb07b9e8f680e0a3c0d087a4f
elchinamirov <elcinemirov533@gmail.com>
2023-10-07 17:02:03 +0400

    BeginnerExercise1
    BeginnerExercise1

---
+++ BeginnerExercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class BeginnerExercise1 {
+    public static void main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Salam,Yasinizi yazin : ");
+
+        int age=scanner.nextInt();{
+            System.out.println(Math.abs(age*365));
+
+
+
+
+        scanner.close();
+    }}}
