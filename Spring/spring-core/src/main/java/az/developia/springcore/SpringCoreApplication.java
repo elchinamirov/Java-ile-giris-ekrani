2664529b5f395f4151aa0b6d72a5e55775301a74
elchinamirov <elcinemirov533@gmail.com>
2023-11-21 16:35:25 +0400

    Spring Lesson 1
    spring _lesson_1

--- Spring/spring-core/src/main/java/az/developia/springcore/SpringCoreApplication.java
+++ Spring/spring-core/src/main/java/az/developia/springcore/SpringCoreApplication.java
@@ -2,12 +2,21 @@ package az.developia.springcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(SpringCoreApplication.class, args);
+	Student student = context.getBean(Student.class);
+	System.out.println(student.getName());
+	
+	String[] beans = context.getBeanDefinitionNames();
+	for (String bean : beans) {
+		System.out.println(bean);
 	}
+	}
+	} 
 
-}
