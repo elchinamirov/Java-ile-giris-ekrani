9bec5d8b1d1694e0f341e76de6c91e27be1486b4
elchinamirov <elcinemirov533@gmail.com>
2023-11-28 17:34:47 +0400

    lesson_25

---
+++ Spring/spring-rest/src/main/java/az/developia/springrest/Product.java
@@ -0,0 +1,69 @@
+package az.developia.springrest;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Product {
+	
+	private Integer id; // db
+	private String name; // user
+	private Double cost; // user
+	private Double price; // user
+	private String barcode; // user
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDateTime register; // bar code
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getCost() {
+		return cost;
+	}
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public LocalDateTime getRegister() {
+		return register;
+	}
+	public void setRegister(LocalDateTime register) {
+		this.register = register;
+	}
+	public Product(Integer id, String name, Double cost, Double price, String barcode, LocalDateTime register) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cost = cost;
+		this.price = price;
+		this.barcode = barcode;
+		this.register = register;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", cost=" + cost + ", price=" + price + ", barcode=" + barcode
+				+ ", register=" + register + "]";
+	}
+	
+	
+
+}
---
+++ Spring/spring-rest/src/main/java/az/developia/springrest/ProductController.java
@@ -0,0 +1,35 @@
+package az.developia.springrest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/products")
+public class ProductController {
+@PostMapping
+public void addProduct(@RequestBody Product product) {
+	// add this object to database
+	// id must be generated on db
+	// register must config
+	
+	product.setRegister(LocalDateTime.now());
+	System.out.println(product);
+}
+@GetMapping
+public List <Product> findAll(){
+	List<Product> products=new ArrayList<Product>();
+	products.add(new Product(2, "Alma", 3D, 7D, "002", LocalDateTime.of(2020, 3, 15, 11, 15,0)));
+	products.add(new Product(5, "Alca", 5D, 7D, "003", LocalDateTime.of(2020, 3, 15, 11, 15,0)));
+	products.add(new Product(6, "Nar", 3D, 7D, "004", LocalDateTime.of(2020, 3, 15, 11, 15,0)));
+	products.add(new Product(6, "Nar", 3D, 7D, "004", LocalDateTime.of(2020, 3, 15, 11, 15,0)));
+
+	return products;
+	}
+}
