58a4de4ee1b93e875fbfd06721345d3a0fce4b5c
elchinamirov <elcinemirov533@gmail.com>
2024-01-17 18:47:35 +0400

    html file has created(6)

---
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/HomeController.java
@@ -0,0 +1,17 @@
+package az.developia.bookshopping.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping(path = "/home")
+	public String showHomePage() {
+		return "home";
+	}
+		
+	
+	
+
+}
