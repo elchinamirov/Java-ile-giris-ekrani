b5ffc86537cf6513f343950aa8462ee4e4546caf
elchinamirov <elcinemirov533@gmail.com>
2024-01-31 16:11:22 +0400

    Spring PathVariable annotasiyasının istifadə olunması . findById( id )
    010

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookRestController.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookRestController.java
@@ -1,10 +1,13 @@
 package az.developia.bookshopping.cotroller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import az.developia.bookshopping.dao.BookDAO;
 import az.developia.bookshopping.model.Book;
 
@@ -17,6 +20,10 @@ public class BookRestController {
 	
     @GetMapping
     public List<Book> findAll(){
-	return bookDAO.findAll();
+	return bookDAO.findAll();}
+	
+	@GetMapping(path = "/{id}")
+    public Book findById(@PathVariable(name="id")Integer id){
+	return bookDAO.findById(id).get();
 }
 }
