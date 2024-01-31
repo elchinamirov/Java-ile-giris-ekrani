46192e697b106147f5b539275276ddc62cb41cf0
elchinamirov <elcinemirov533@gmail.com>
2024-01-31 14:47:42 +0400

    Spring REST controller yaratmaq addımları
    009.

---
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/dao/BookDAO.java
@@ -0,0 +1,11 @@
+package az.developia.bookshopping.dao;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import az.developia.bookshopping.model.Book;
+
+public interface BookDAO extends JpaRepository<Book, Integer> {
+
+}
