f0610bd89fbd0883c8add6e904c6a037c30a93d5
elchinamirov <elcinemirov533@gmail.com>
2024-02-02 00:53:55 +0400

    019
    Səhifələrə keçid linkləri əlavə etmək

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/HomeController.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/HomeController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping(path = "/home")
+	@GetMapping(path = {"/home","/"})
 	public String showHomePage() {
 		return "home";
 	}
+	
 		
 	
 	
