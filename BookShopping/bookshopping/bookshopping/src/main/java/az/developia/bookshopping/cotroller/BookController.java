89b27499e87b17ce9938f9ef545c7ae44a0927e8
elchinamirov <elcinemirov533@gmail.com>
2024-02-01 18:36:00 +0400

    014
    Yeni kitab qeydiyyatı addımı

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookController.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookshopping.dao.BookDAO;
 import az.developia.bookshopping.model.Book;
@@ -24,4 +27,40 @@ public class BookController {
                         return "books";
 			
 		}
+		@GetMapping(path = "/books/new")
+		public String openNewBookPage (Model model) {
+			Book book=new Book();
+			model.addAttribute("book", book);
+                        return "new-book";
+			
 		}
+		
+		
+		
+		@PostMapping(path = "/books/new-book-process")
+		public String saveBook(@ModelAttribute(name="book") Book book,Model model) {
+			bookDAO.save(book);
+			List<Book> books= bookDAO.findAll();
+			model.addAttribute("books", books);
+			return "books";
+			
+		}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
@@ -13,6 +13,8 @@ public class Book {
 	
 	private Integer id;
 	private String name;
+	private String description;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -25,5 +27,12 @@ public class Book {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	
 }
