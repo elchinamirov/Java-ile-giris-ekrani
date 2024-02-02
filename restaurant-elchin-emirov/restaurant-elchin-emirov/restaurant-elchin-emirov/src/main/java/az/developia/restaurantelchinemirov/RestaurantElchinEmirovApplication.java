6f1f467f5849c892def1fa9302ccd5b68e030a9e
elchinamirov <elcinemirov533@gmail.com>
2024-02-02 13:27:29 +0400

    restaurant project created

---
+++ restaurant-elchin-emirov/restaurant-elchin-emirov/restaurant-elchin-emirov/src/main/java/az/developia/restaurantelchinemirov/RestaurantElchinEmirovApplication.java
@@ -0,0 +1,13 @@
+package az.developia.restaurantelchinemirov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantElchinEmirovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantElchinEmirovApplication.class, args);
+	}
+
+}
