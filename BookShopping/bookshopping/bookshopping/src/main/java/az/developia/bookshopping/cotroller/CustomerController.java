074a8411975b420bd3ceca6128cadcea93eacb22
elchinamirov <elcinemirov533@gmail.com>
2024-02-05 13:18:27 +0400

    032
    Digər səhifələrin yaradılması

---
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/CustomerController.java
@@ -0,0 +1,48 @@
+package az.developia.bookshopping.cotroller;
+
+import java.util.List;
+
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import az.developia.bookshopping.dao.BookDAO;
+import az.developia.bookshopping.model.Book;
+import ch.qos.logback.core.joran.conditional.IfAction;
+
+
+@Controller
+public class CustomerController {
+
+		
+		@GetMapping(path = "/customer")
+		public String showCustomerPage () {
+			
+                        return "customer";
+			
+		}
+		
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
