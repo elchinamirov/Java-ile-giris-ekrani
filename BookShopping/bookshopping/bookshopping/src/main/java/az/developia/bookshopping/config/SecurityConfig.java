1e622b671d0682141cdfb8dd69d4007845f0af2e
elchinamirov <elcinemirov533@gmail.com>
2024-02-06 20:15:25 +0400

    035. Xüsusi login pəncərəsi yaratmaq(2)

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
@@ -1,31 +1,37 @@
 package az.developia.bookshopping.config;
 
-public class SecurityConfig {
-
-	import javax.sql.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.web.SecurityFilterChain;
 
-	@Configuration
-	@EnableWebSecurity
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-			 @Override
-			protected void configure(HttpSecurity http) throws Exception {
-				http.csrf().disable().authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/").permitAll()
-				.anyRequest().authenticated()
-				.and().formLogin().loginPage("/show-login")
-				.loginProcessingUrl("/authenticate-user").permitAll()
-				.and().logout().permitAll();
-				
-			 } 
-			 
-			 
-	}
+		 @Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable().authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/").permitAll()
+			.anyRequest().authenticated().and()
+			.formLogin().loginPage("/show-login")
+			.loginProcessingUrl("/authenticate-user").permitAll()
+			.and().logout().permitAll();
+			
+		 } 
+		 
+	
+}
