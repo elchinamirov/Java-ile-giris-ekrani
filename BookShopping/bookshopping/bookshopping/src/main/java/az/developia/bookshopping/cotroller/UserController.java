a9f03f2a683c50404d50e9a071cc5cdd3f550d8a
elchinamirov <elcinemirov533@gmail.com>
2024-02-06 19:52:49 +0400

    035. Xüsusi login pəncərəsi yaratmaq

---
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/UserController.java
@@ -0,0 +1,18 @@
+package az.developia.bookshopping.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping(path = {"/show-login"})
+	public String showLoginPage() {
+		return "my-custom-login";
+	}
+	
+		
+	
+	
+
+}
