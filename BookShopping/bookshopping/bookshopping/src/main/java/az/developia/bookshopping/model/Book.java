2b641adcf343e183222c847ac32e96aa7ec71286
elchinamirov <elcinemirov533@gmail.com>
2024-02-08 19:36:44 +0400

    041. Kitabın qiymətinin boş qoyulmasının qarşısının alınması

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/model/Book.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -15,15 +18,20 @@ public class Book {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
 	private Integer id;
+	
 	@NotEmpty(message="Boş qoymaq olmaz")
 	@Size(min=2,max=50,message = "Ən azı 2 simvol yazmaq lazımdır")
 	@Size(max=50,message ="Ən çoxu 50 simvol yazmaq lazımdır")
 	@Column(columnDefinition = "VARCHAR(50)")
 	private String name;
+	
 	@Column(columnDefinition = "VARCHAR(300)")
 	@Size(max=300,message ="Ən çoxu 300 simvol yazmaq lazımdır")
-
 	private String description;
+	
+	@Min(value = 0,message = "Ən azı 0 yazmaq olar")
+	@Max(value = 1000,message = "Ən çoxu 1000 yazmaq olar")
+	@NotNull(message = "Boş qoymaq olmaz")
 	private Double price;
 	private String author;
 	private Integer pageCount;
