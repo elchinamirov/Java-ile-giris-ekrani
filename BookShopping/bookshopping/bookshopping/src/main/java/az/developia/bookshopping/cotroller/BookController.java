26975d3f19579f654f96c45f396dc1d9a0cd433d
elchinamirov <elcinemirov533@gmail.com>
2024-02-09 17:20:05 +0400

    058. Kitabların istifadəçi adına görə filtr olunması

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookController.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/BookController.java
@@ -31,7 +31,8 @@ public class BookController {
 		
 		@GetMapping(path = "/books")
 		public String showBooks (Model model) {
-			List<Book> books=bookDAO.findAll();
+			//List<Book> books=bookDAO.findAll();
+			List<Book> books=bookDAO.findAllByUsername(mySession.getUsername());
 						 model.addAttribute("books", books);
                         return "books";
 			
--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/dao/BookDAO.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/dao/BookDAO.java
@@ -2,10 +2,12 @@ package az.developia.bookshopping.dao;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import az.developia.bookshopping.model.Book;
 
 public interface BookDAO extends JpaRepository<Book, Integer> {
-
+public List<Book> findAllByUsername(String username);
 }
