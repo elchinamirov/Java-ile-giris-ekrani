1a0b5b8172ff91f338709a925e188165724d1b4f
elchinamirov <elcinemirov533@gmail.com>
2024-02-24 15:46:16 +0400

    061. Yeni istifadəçi yaratmaq son addım

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/UserController.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/cotroller/UserController.java
@@ -2,6 +2,7 @@ package az.developia.bookshopping.cotroller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,10 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import az.developia.bookshopping.dao.UserDAO;
 import az.developia.bookshopping.model.User;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private boolean userCreated=false;
 	
 	@GetMapping(path = {"/show-login"})
@@ -35,9 +49,15 @@ public class UserController {
 		    if (result.hasErrors()) {
 			return "create-account";
 		}
-		System.out.println(user);
+		//System.out.println(user);
+		   boolean userExists=userDAO.createUser(user);
+		   if(userExists) {
+			   model.addAttribute("userExists", "");
+				return "create-account";
+		   }
+			   
         userCreated=true;
-return "my-custom-login";
+return "redirect:/show-login";
 		
 	}
 	
