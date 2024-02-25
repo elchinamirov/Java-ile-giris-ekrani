bc31403936b3ef2dd663f583b830cd46516f858b
elchinamirov <elcinemirov533@gmail.com>
2024-02-25 20:59:33 +0400

    064. AJAX ilə REST servisi istifadə etmək

--- BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SwaggerConfig.java
+++ BookShopping/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/config/SwaggerConfig.java
@@ -2,8 +2,7 @@ package az.developia.bookshopping.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
+//import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
