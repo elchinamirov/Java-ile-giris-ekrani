1138ab2bfaa81f978cb972bba08932e41e58d556
elchinamirov <elcinemirov533@gmail.com>
2024-02-26 14:54:33 +0400

    002. Tələbə qeydiyyatı rest servisini yazdıq

---
+++ student-crud/student-crud/student-crud/src/main/java/az/developia/studentcrud/controller/StudentController.java
@@ -0,0 +1,36 @@
+package az.developia.studentcrud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import az.developia.studentcrud.model.Student;
+import az.developia.studentcrud.repository.StudentRepository;
+
+@RestController
+@RequestMapping(path = "/students")
+@CrossOrigin(origins = "*")
+public class StudentController {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@PostMapping
+	public Student save(@RequestBody Student student) {
+		return studentRepository.save(student);
+	}
+	
+	@GetMapping
+	public List<Student> findAlL(){
+		return studentRepository.findAll();
+		
+	}
+	
+	
+}
