b6a3842162a069fb8b1c34716e36d2ad85b950cb
elchinamirov <elcinemirov533@gmail.com>
2024-03-05 00:19:28 +0400

    ok2

--- restaurant-elchin-emirov/restaurant-elchin-emirov/restaurant-elchin-emirov/src/main/java/az/developia/restaurantelchinemirov/service/ProductService.java
+++ restaurant-elchin-emirov/restaurant-elchin-emirov/restaurant-elchin-emirov/src/main/java/az/developia/restaurantelchinemirov/service/ProductService.java
@@ -1,8 +1,31 @@
 package az.developia.restaurantelchinemirov.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-@Service
+import org.springframework.validation.BindingResult;
 
+import az.developia.restaurantelchinemirov.dao.ProductDAO;
+import az.developia.restaurantelchinemirov.exception.OurRuntimeException;
+import az.developia.restaurantelchinemirov.model.Product;
+@Service
 public class ProductService {
 
+	@Autowired
+	private ProductDAO productDAO;
+	
+	public void add(Product p, BindingResult br) {
+		if (br.hasErrors()) {
+			throw new OurRuntimeException("Məlumatların tamlığı pozulub", br);
+			
+		}
+		p.setId(p.getId());
+		p.setName(p.getName());
+		p.setQuantity(p.getQuantity());
+        
+		productDAO.save(p);
+	}
+	
+	public void update (Product) {
+		
+	}
 }
